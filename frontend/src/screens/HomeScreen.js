0040aab76497a8aff8edb54d392c26a2cd30fce9
duke viet <dukeviet@gmail.com>
2021-06-21 16:05:21 +0700

    duc

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+function HomeScreen(props) {
+
+    const [products, setProducts] = useState([]);
+
+    useEffect(()=>{
+        const fetchData = async () => {
+            const {data} = await axios.get("/api/products");
+            setProducts(data);
+        }
+        fetchData();
+        return () =>{
+
+        };
+    },[]);
+  return (
+    <ul className="products">
+      {products.map((product) => (
+        <li key={product._id}>
+          <div className="product">
+            <Link to={"/products/" + product._id}>
+              {" "}
+              <img
+                className="product-image"
+                src={product.image}
+                alt="product"
+              />{" "}
+            </Link>
+            <div className="product-name">
+              <Link to={"/products/" + product._id}> {product.name} </Link>
+            </div>
+            <div className="product-brand">{product.brand}</div>
+            <div className="product-price">$ {product.price}</div>
+            <div className="product-rating">
+              {" "}
+              {product.rating} Stars ( {product.numReviews} Reviews)
+            </div>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+}
+export default HomeScreen;
---
+++ frontend/src/store.js
@@ -0,0 +1,11 @@
+import {createStore, combineReducers} from 'redux';
+import { productListReducer } from '../reducers/productReducers';
+
+const initialState = {};
+const reducer = combineReducers(
+    {
+        productList : productListReducer,
+    }
+)
+
+const store = createStore(reducer, initialState)
